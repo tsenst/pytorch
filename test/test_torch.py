4d6a8c21fd1f32720b04869178dd7acc1df43e1e
Alban Desmaison <albandes@fb.com>
2020-05-13 11:08:10 -0400

    Add utility to enable cpp stacktraces in torch.utils.debug
    ghstack-source-id: f8a480297f2987caa16ef779390c5d2781ddf11e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/38127

--- test/test_torch.py
+++ test/test_torch.py
@@ -5190,6 +5190,33 @@ tensor([[[1., 1., 1.,  ..., 1., 1., 1.],
             model.weight.data = weight
             out = model(input)
 
+    def test_cpp_stacktraces(self):
+        # These messages can be changed if the `torch.rand` implementation is changed.
+        # They are just here to make sure that the stack traces are in the message.
+        python_error_msg = "Trying to create tensor with negative dimension"
+        cpp_stack_msg = "Exception raised from check_size_nonnegative at"
+
+        def check_error(has_cpp_stack_trace):
+            raised = False
+            try:
+                torch.rand(-1)
+            except Exception as e:
+                raised = True
+                err_msg = str(e)
+                self.assertTrue(python_error_msg in err_msg)
+                self.assertTrue((cpp_stack_msg in err_msg) == has_cpp_stack_trace)
+
+            self.assertTrue(raised)
+
+        orig = torch.utils.debug.is_cpp_stacktraces_enabled()
+        try:
+            torch.utils.debug.set_cpp_stacktraces_enabled(False)
+            check_error(False)
+            torch.utils.debug.set_cpp_stacktraces_enabled(True)
+            check_error(True)
+        finally:
+            torch.utils.debug.set_cpp_stacktraces_enabled(orig)
+
 
 # Functions to test negative dimension wrapping
 METHOD = 1
